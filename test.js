36a27edd9506c1aaf6b591046ba1cb9048924387
jinjing xie <shinenext@hotmail.com>
2014-09-17 22:52:11 +0800

    add a test js file

---
+++ test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by xiejinjing on 9/17/14.
+ */
+function test() {
+    var a1, a2,
+        a3;
+    if (a1 == 1) {
+        console.log("1");
+    } else {
+        console.log("2");
+    }
+
+}
